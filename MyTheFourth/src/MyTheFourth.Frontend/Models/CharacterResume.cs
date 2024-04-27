d29d6293884245daff1550db8e9cde62269cf4a9
Paulinho Santana <paulo.santana@montrealviagens.com>
2024-04-27 11:13:22 -0300

    refactor: remove json naming for models

--- MyTheFourth/src/MyTheFourth.Frontend/Models/CharacterResume.cs
+++ MyTheFourth/src/MyTheFourth.Frontend/Models/CharacterResume.cs
@@ -1,13 +1,9 @@
-using System.Text.Json.Serialization;
-
 namespace MyTheFourth.Frontend.Models;
 
 
 public class CharacterResume : AbstractItemModel
 {
-    [JsonPropertyName("id")]
     public int Id { get; set; }
 
-    [JsonPropertyName("title")]
     public string Name { get; set; } = null!;
 }
--- MyTheFourth/src/MyTheFourth.Frontend/Models/MovieResume.cs
+++ MyTheFourth/src/MyTheFourth.Frontend/Models/MovieResume.cs
@@ -1,12 +1,8 @@
-using System.Text.Json.Serialization;
-
 namespace MyTheFourth.Frontend.Models;
 
 public class MovieResume : AbstractItemModel
 {
-    [JsonPropertyName("id")]
     public int Id { get; set; }
 
-    [JsonPropertyName("title")]
     public string Title { get; set; } = null!;
 }
