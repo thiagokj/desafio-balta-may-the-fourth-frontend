1d1ffbd17bd69c33c37522e0f5875cb07c97aaf3
Vitor Renno <vitorrenno@outlook.com>
2024-04-30 21:32:26 -0300

    .

--- MyTheFourth/src/MyTheFourth.Frontend/Services/Interfaces/ICharactersService.cs
+++ MyTheFourth/src/MyTheFourth.Frontend/Services/Interfaces/ICharactersService.cs
@@ -4,7 +4,7 @@ namespace MyTheFourth.Frontend.Services.Interfaces;
 
 public interface ICharactersService
 {
-
     public Task<Character?> GetCharacterAsync(string characterId);
-    public Task<IEnumerable<Character>> ListCharactersAsync(int? page = null, int? pageSize = null);
+    //public Task<IEnumerable<Character>> ListCharactersAsync(int? page, int? pageSize = 0);
+    public Task<IEnumerable<Character>> ListCharactersAsync(int? page, int? pageSize = 0);
 }
